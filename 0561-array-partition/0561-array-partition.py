b4efb5094017edd98c498a51d077f8fbecd1cd70
Amritesh Das <95065530+amriteshdas@users.noreply.github.com>
2026-08-14 17:06:07 +0530

    Time: 47 ms (12.88%), Space: 14 MB (8.87%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -3,7 +3,7 @@ class Solution(object):
         snum=sorted(nums)
         msum=0
         i = 0
-        for _ in range(len(nums)/2):
+        for _ in range(len(nums)//2):
             msum=msum+min(snum[i],snum[i+1])
             i = i+2
         return msum
